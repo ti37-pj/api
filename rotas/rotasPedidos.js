47a34b121c55109bb09cadb0638e15508933de3e
Kauaflp <fillipie11@gmail.com>
2022-11-01 15:30:19 -0300

    Opção de buscar Pedidos pela Mesa ou Usuário

--- rotas/rotasPedidos.js
+++ rotas/rotasPedidos.js
@@ -4,9 +4,14 @@ const caminho = ( metodo ) => require("../controlador/pedidos/"+ metodo);
 // GET
 rotas.get("/busca_todos", caminho("get").BuscaTodos );
 rotas.get("/busca/:id", caminho("get").Busca);
+rotas.get("/busca_usuario/:id", caminho("get").BuscaUsuario);
+rotas.get("/busca_mesa/:mesa", caminho("get").BuscaMesa);
 
 // POST
 rotas.post("/insere", caminho("post").Insere );
+
+// PUT
+rotas.put("/altera/:id", caminho("put").Altera );
 
 // DELETE
 // rotas.delete("/:id", caminho("delete").Remove );

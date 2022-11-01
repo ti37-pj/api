28bcbbc8489eb34322a77f6eaf660ab0d71c0585
Kauaflp <fillipie11@gmail.com>
2022-11-01 15:30:32 -0300

    Rota de Autenticação

--- rotas/rotasFuncionarios.js
+++ rotas/rotasFuncionarios.js
@@ -1,5 +1,5 @@
 const rotas = require("express").Router();
-const caminho = ( metodo ) => require("../controlador/funcionarios/"+ metodo);
+const caminho = ( metodo ) => require("../controlador/usuarios/"+ metodo);
 
 // GET
 rotas.get("/busca_todos", caminho("get").BuscaTodos );
@@ -7,6 +7,7 @@ rotas.get("/busca/:id", caminho("get").Busca);
 
 // POST
 rotas.post("/insere", caminho("post").Insere );
+rotas.post("/autentica", caminho("post").Autentica );
 
 // DELETE
 rotas.delete("/deleta/:id", caminho("delete").Deleta );
